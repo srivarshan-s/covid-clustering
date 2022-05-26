e84e918d1498d5ab9bbabc141856269d5fa4e12f
Srivarshan <srivarshan02@gmail.com>
2022-05-26 10:21:38 -0600

    Create main.R

---
+++ src/main.R
@@ -0,0 +1,22 @@
+# Environment Setup
+setwd("~/Documents/covid-clustering")
+
+# Import packages
+library(readr)
+library(dplyr)
+library(tidyverse)
+
+# Importing the data
+df <- read_csv("data/owid_covid_data.csv")
+
+# Print the data
+# print(df)
+
+# Select only the required columns
+df <- df %>% 
+    select(
+        "iso_code", "continent", "location", "date",
+        "new_cases_per_million", "new_deaths_per_million",
+        "total_cases_per_million", "total_deaths_per_million",
+        "stringency_index", "new_tests_per_thousand")
+print(df)
