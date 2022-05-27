5b1ac285e9c70f660cc5114cd0d57225ace88295
Srivarshan <srivarshan02@gmail.com>
2022-05-27 09:55:05 -0600

    Plot functional data

--- src/main.R
+++ src/main.R
@@ -14,18 +14,33 @@ library("readr")
 
 # Read train data
 df_train <- read_tsv("data/ECG200_TRAIN.tsv", col_names = FALSE)
-# print(df_train)
+print("Training data")
+print(df_train)
 
 # Read test data
 df_test <- read_tsv("data/ECG200_TEST.tsv", col_names = FALSE)
-# print(df_test)
+print("Testing data")
+print(df_test)
 
 # Merging the two dataframes
 df <- rbind(df_train, df_test)
-# print(df)
+print("Merged data")
+print(df)
 
 # Extracting the two classes
 df_class_1 <- filter(df, X1 == "-1")
 df_class_2 <- filter(df, X1 == "1")
+print("Class 1")
 print(df_class_1)
+print("Class 2")
 print(df_class_2)
+
+# Plot class 1
+ggplot(data = df_class_1) + 
+	geom_line(mapping = aes(
+		x = colnames(df_class_1),
+		y = data.matrix(df_class_1[1, ])
+	))
+
+print(colnames(df_class_1))
+print(data.matrix(df_class_1[1, ]))
