4b12bfb2ee1b30810c27a93dbd7c89ed37cadeb0
Srivarshan <srivarshan02@gmail.com>
2022-06-14 15:27:11 -0600

    Create plots for all data points

--- src/plot_functional_data.R
+++ src/plot_functional_data.R
@@ -20,35 +20,35 @@ NSPLINE_BSPLINE <- 20
 #################### DEFINE FUNCTIONS ##########################
 
 set_seed <- function() {
-  set.seed(14071)
+    set.seed(14071)
 }
 
 fourier_basis <- function(data) {
-  set_seed()
-  basis <- fda::create.fourier.basis(
-    rangeval = c(0, 95),
-    nbasis = NBASIS_FOURIER
-  )
-  ecg_fdata <- smooth.basis(
-    argvals = seq(0, 95, length.out = 96),
-    y = t(data.matrix(data)),
-    fdParobj = basis
-  )$fd
-  return(ecg_fdata)
+    set_seed()
+    basis <- fda::create.fourier.basis(
+                                       rangeval = c(0, 95),
+                                       nbasis = NBASIS_FOURIER
+    )
+    ecg_fdata <- smooth.basis(
+                              argvals = seq(0, 95, length.out = 96),
+                              y = t(data.matrix(data)),
+                              fdParobj = basis
+                              )$fd
+    return(ecg_fdata)
 }
 
 bspline_basis <- function(data) {
-  set_seed()
-  basis <- create.bspline.basis(
-    rangeval = c(0, 95), 
-    nbasis = NSPLINE_BSPLINE
-  )
-  ecg_fdata <- smooth.basis(
-    argvals = seq(0, 95, length.out = 96),
-    y = t(data.matrix(data)),
-    fdParobj = basis
-  )$fd
-  return(ecg_fdata)
+    set_seed()
+    basis <- create.bspline.basis(
+                                  rangeval = c(0, 95), 
+                                  nbasis = NSPLINE_BSPLINE
+    )
+    ecg_fdata <- smooth.basis(
+                              argvals = seq(0, 95, length.out = 96),
+                              y = t(data.matrix(data)),
+                              fdParobj = basis
+                              )$fd
+    return(ecg_fdata)
 }
 
 
@@ -76,12 +76,15 @@ fourier_data <- fourier_basis(plot_df)
 bspline_data <- bspline_basis(plot_df)
 
 # Plot data
-pdf("norm_vs_func.pdf", height = 100, width = 100)
-plot.fd(
-  fourier_data[1],
-  col = 2,
-  xlab = "Time",
-  ylab = "Value",
-)
-lines(normal_data[1, ], col = 1)
-lines(bspline_data[1], col = 3)
+for (idx in 151:200) {
+    pdf_name <- paste("norm_vs_func_", as.character(idx), ".pdf", sep = "")
+    pdf(pdf_name)
+    plot.fd(
+            fourier_data[idx],
+            col = 2,
+            xlab = "Time",
+            ylab = "Value",
+    )
+    lines(normal_data[idx, ], col = 1)
+    lines(bspline_data[idx], col = 3)
+}
